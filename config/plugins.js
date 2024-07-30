d42a6d6fac823d9d0a882a65617ccae76b150d77
Roman Smolin <romasmolin29@gmail.com>
2024-07-30 20:18:15 +0300

    Adjust config

--- config/plugins.js
+++ config/plugins.js
@@ -16,4 +16,17 @@ module.exports = ({ env }) => ({
         },
       },
     },
+    graphql: {
+      enabled: true,
+      config: {
+        endpoint: "/graphql",
+        shadowCRUD: true,
+        playgroundAlways: true,
+        defaultLimit: 10,
+        maxLimit: 20,
+        apolloServer: {
+          tracing: true,
+        },
+      },
+    },
   });

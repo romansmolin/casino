e64432698d30459d3dbfd33e007f5ec015f47ef9
romansmolin <roman.smolin@fintecor.com>
2025-02-18 14:54:17 +0200

    Newsletter

--- src/graphql/graphql.js
+++ src/graphql/graphql.js
@@ -5,7 +5,8 @@ const casino = require('../api/casino/graphql')
 const bonuses = require('../api/bonus/graphql')
 const pages = require('../api/page/graphql')
 const globalSearch = require('../api/search/graphql')
-const extensions = [top, casino, bonuses, pages, globalSearch]
+const newsletter = require('../api/newsletter/graphql')
+const extensions = [top, casino, bonuses, pages, globalSearch, newsletter]
 
 module.exports = (strapi) => {
   const extensionService = strapi.plugin('graphql').service('extension')

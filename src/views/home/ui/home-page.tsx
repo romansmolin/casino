a37d860027d2452002420f47d36bc12fbdfdc41d
Роман Смолин <romansmolin@Romans-MacBook-Pro.local>
2025-03-10 16:01:05 +0200

    Stop using typography for mian title

--- src/views/home/ui/home-page.tsx
+++ src/views/home/ui/home-page.tsx
@@ -74,9 +74,7 @@ const HomePage = async ({ locale }: { locale: Locale }) => {
     return (
         <>
             <section className="space-y-5 flex-1 bento-block">
-                <Typography as="h1" variant='h1'>
-                    {t('title')} 2024
-                </Typography>
+                <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-left">{t('title')} 2024</h1>
                 <CasinoTop byCountry='Germany' />
             </section>
 

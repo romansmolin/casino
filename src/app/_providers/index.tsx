e6850a23c6538ce64e92888d7dc3ceb8bb3e7664
Роман Смолин <romansmolin@Romans-MacBook-Pro.local>
2025-03-10 14:38:34 +0200

    Lazy load apollo provider

--- src/app/_providers/index.tsx
+++ src/app/_providers/index.tsx
@@ -1,12 +1,17 @@
 import React, { ReactNode } from 'react'
 import { ThemeProvider } from './theme-provider'
-import { ApolloProvider } from './apollo-provider'
+// import { ApolloProvider } from './apollo-provider'
 import { SidebarProvider } from '@/shared/ui/sidebar'
 import { NextIntlClientProvider } from 'next-intl'
 import { getMessages } from 'next-intl/server'
+import dynamic from 'next/dynamic'
+
 interface ProvidersProps {
     children: ReactNode
 }
+
+const ApolloProvider = dynamic(() => import('./apollo-provider').then((mod) => mod.ApolloProvider))
+
 
 const Providers: React.FC<ProvidersProps> = async ({ children }) => {
 

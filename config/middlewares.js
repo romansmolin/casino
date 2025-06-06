b73be5a020b00f4e8a7c089107f0bae6b22c90e0
romansmolin <romasmolin29@gmail.com>
2025-06-06 15:32:51 +0300

    S3

--- config/middlewares.js
+++ config/middlewares.js
@@ -8,13 +8,16 @@ module.exports = [
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com',                 
+          'aff-site-demo-bucket.s3.eu-north-1.amazonaws.com',
+          ],
           'media-src': [
             "'self'",
             'data:',
             'blob:',
             'market-assets.strapi.io',
             'res.cloudinary.com',
+            'aff-site-demo-bucket.s3.eu-north-1.amazonaws.com',
           ],
           "frame-src": [ "http://localhost:*", "self", "sandbox.embed.apollographql.com" ],
           upgradeInsecureRequests: null,
--- config/plugins.js
+++ config/plugins.js
@@ -19,6 +19,27 @@ module.exports = ({ env }) => ({
   //   },
   // },
 
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
+        region: env('AWS_REGION'),
+        params: {
+          ACL: env('AWS_ACL', 'public-read'),
+          signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+          Bucket: env('AWS_S3_BUCKET'),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+
   graphql: {
     enabled: true,
     config: {
